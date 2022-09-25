2184d0cf51f8aa25458cd0915718b29dce5b64cb
peteykienzzz <pmatczak2@gmail.com>
2022-09-25 13:57:54 -0700

    Reverse array in groups

--- find_subarray_with_given_sum.py
+++ find_subarray_with_given_sum.py
@@ -9,23 +9,26 @@ def sub_array_sum(arr, n, sum_):
     current_sum = arr[0]
     start = 0
     i = 1
-
     while i <= n:
-
         while current_sum > sum_:
             current_sum = current_sum - arr[start]
             start += 1
 
         if current_sum == sum_:
-            print(f"sum is in index {start} and {i - 1}")
+            print(f"sum in in index between {start} and {i - 1}")
             return 1
 
         if i < n:
             current_sum = current_sum + arr[i]
+
+
         i += 1
 
-    print("No subarray found")
+
+
+    print("Subarray in not in array")
     return 0
+
 arr = [15, 2, 4, 8, 9, 5, 10, 23]
 n = len(arr)
 sum_ = 23
--- two_lists.py
+++ two_lists.py
@@ -1,3 +1,15 @@
+# Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
+# Note: If at any instance, there are no more subarrays of size greater than or equal to K, then reverse the last subarray
+# (irrespective of its size).
+# Input:
+# N = 5, K = 3
+# arr[] = {1,2,3,4,5}
+# Output: 3 2 1 5 4
+# Explanation: First group consists of elements
+# 1, 2, 3. Second group consists of 4,5.
+
+
+
 def reverse(array, n, k):
     i = 0
     while i < n:
@@ -13,9 +25,9 @@ def reverse(array, n, k):
         i += k
 
 
-arr = [1, 2, 3, 4, 5, 6]
+arr = [1, 2, 3, 4, 5]
 
-k = 2
+k = 3
 n = len(arr)
 reverse(arr, n, k)
 
