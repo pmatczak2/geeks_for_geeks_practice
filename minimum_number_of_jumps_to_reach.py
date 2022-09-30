79bc8b351655392e495d83f86c57c35c54ab9452
peteykienzzz <pmatczak2@gmail.com>
2022-09-30 16:01:32 -0700

    Minimum number of jumps to reach end / Naive Recursive Approach.

--- minimum_number_of_jumps_to_reach.py
+++ minimum_number_of_jumps_to_reach.py
@@ -10,8 +10,31 @@
 # If 8 or 9 is chosen then the end node 9
 # can be reached. So 3 jumps are made.
 def min_jumps(arr, n):
+    if n <= 1:
+        return 0
 
+    if arr[0] == 0:
+        return -1
 
+    max_reach = arr[0]
+    step = arr[0]
+    jump = 1
+
+    for i in range(1, n):
+        if i == n-1:
+            return jump
+
+        max_reach = max(max_reach, i + arr[i])
+        step -= 1
+
+        if step == 0:
+            jump += 1
+
+            if i >= max_reach:
+                return -1
+
+            step = max_reach - i
+    return -1
 
 arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
 size = len(arr)
