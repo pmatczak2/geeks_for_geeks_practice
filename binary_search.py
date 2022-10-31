2b4790ffd3690a1bf785ec0bb969765cbd9bdff0
peteykienzzz <pmatczak2@gmail.com>
2022-10-31 04:21:44 -0700

    Given a sorted array arr[] of n elements, write a function to search a given element x in arr[] and return the index of x in the array.

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,18 @@
-def sorting(array, left_index, right_index, x):
+# Problem: Given a sorted array arr[] of n elements, write a function to search a given element x in arr[] and return
+# the index of x in the array.
+#
+#                  Consider array is 0 base index.
+#
+# Examples:
+#
+# Input: arr[] = {10, 20, 30, 50, 60, 80, 110, 130, 140, 170}, x = 110
+# Output: 6
+# Explanation: Element x is present at index 6.
+
+
+
+
+def binary_search(array, left_index, right_index, x):
 
     while left_index <= right_index:
         middle = left_index + right_index - 1 // 2
@@ -13,7 +27,12 @@ def sorting(array, left_index, right_index, x):
             right_index = middle - 1
 
     return -1
-arr = [1,2,3,4]
-x = 8
+arr = [2, 3, 4, 10, 40]
+x = 10
 
-print(sorting(arr, 0, len(arr) - 1, x))
+result = binary_search(arr, 0, len(arr) -1, x)
+
+if result != -1:
+    print(f"Element is present at index {result}")
+else:
+    print("Element in not present in array")
